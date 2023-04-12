95fdb4289c1a497516ea9866aaaefd49711fc2b1
Juan Peñalba <juantandil123@gmail.com>
2023-04-12 12:36:22 -0300

    finish

--- scripts/main.js
+++ scripts/main.js
@@ -1,33 +1,29 @@
+if (localStorage.getItem("puntos") === null) {
+  let puntos = [];
+  localStorage.setItem("puntos", JSON.stringify(puntos));
+}
 
-
-
-if(localStorage.getItem('puntos') === null) {
-    let puntos = [];
-    localStorage.setItem('puntos', JSON.stringify(puntos));
-} 
-
-
-const btnIniciar = document.querySelector('.btn-iniciar');
-const btnReiniciar = document.querySelector('.btn-reiniciar');
+const btnIniciar = document.querySelector(".btn-iniciar");
+const btnReiniciar = document.querySelector(".btn-reiniciar");
 let requestID;
 let fotogramasPorSegundo = 8;
 const G = new Game();
 
-btnIniciar.addEventListener('click', ()=> {
-    G.iniciar()
-    loop()
-    btnIniciar.style.display = 'none';    
+btnIniciar.addEventListener("click", () => {
+  G.iniciar();
+  loop();
+  btnIniciar.style.display = "none";
 });
 
-btnReiniciar.addEventListener('click', ()=> {
-    G.reiniciar();  
-    loop()
-    btnReiniciar.style.display = 'none';
+btnReiniciar.addEventListener("click", () => {
+  G.reiniciar();
+  loop();
+  btnReiniciar.style.display = "none";
 });
 
 function loop() {
-    setTimeout(function() {
-        requestID = window.requestAnimationFrame(loop);
-        G.moverResultado();
-    }, 1000 / fotogramasPorSegundo);
+  setTimeout(function () {
+    requestID = window.requestAnimationFrame(loop);
+    G.moverResultado();
+  }, 1000 / fotogramasPorSegundo);
 }
